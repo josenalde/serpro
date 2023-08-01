76ba03a40d42ed4a38408ee61d37d1754c75c1a1
josenaldeOliveira <josenalde@gmail.com>
2023-08-01 15:23:05 -0300

    Add files via upload

---
+++ python/count_vowels.py
@@ -0,0 +1,17 @@
+# by Josenalde Oliveira
+import unicodedata
+s = input('string a comparar: ')
+s = s.lower()  # passa para minusculo (retorna string)
+
+
+s = ''.join(ch for ch in unicodedata.normalize('NFKD', s)
+            if not unicodedata.combining(ch))
+
+v = ['a', 'e', 'i', 'o', 'u']
+c_v = 0
+for l in v:
+    c_v += s.count(l)
+
+
+print(s + 'with: \n')
+print(str(c_v) + 'vowels')
