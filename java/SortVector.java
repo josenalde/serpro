998c069bf85d99cfbf64ade5a316aea0572bfb81
josenaldeOliveira <josenalde@gmail.com>
2023-08-21 13:15:01 -0300

    Add files via upload

---
+++ java/SortVector.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Collections;
+class SortVetor {
+  public static void main(String[] args) {
+    int[] v = { 5, -2, 23, 7, 87, -42, 509 };
+    //int[] v = new int[]{1,4,3};
+    Arrays.sort(v);
+    for (int x : v) {
+      System.out.println(x);
+    }
+    Integer[] arr = { 13, 7, 6, 45, 21, 9, 2, 100 };
+    Arrays.sort(arr, Collections.reverseOrder());
+  
+        // Printing the array as generated above
+    System.out.println("Modified arr[] : "
+                           + Arrays.toString(arr));
+  }
+}
