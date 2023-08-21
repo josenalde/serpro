883a61fab1a8d070e1b02d2f25bb6a32bdadfdcd
josenaldeOliveira <josenalde@gmail.com>
2023-08-21 14:37:24 -0300

    Add files via upload

---
+++ javascript/binary_search.js
@@ -0,0 +1,26 @@
+//by Josenalde Oliveira
+function binary_search(v, n) {
+    // n: number to search for
+    let begin, end, middle;
+    begin = 0;
+    end = v.length - 1;
+    while (begin <= end) {
+        middle = Math.floor((begin + end) / 2); //.round(), .ceil()
+        if (v[middle] == n) {
+            return middle;
+        } else if (n < v[middle]) {
+            end = middle - 1;
+        } else {
+            begin = middle + 1;
+        }
+    }
+    return -1; // if not found
+}
+
+s = [1,4,6,9,11,20,100];
+r = [1,6,4,100,20,11,5];
+r.sort(function (a, b) {
+    return a - b;
+});
+console.log(r);
+console.log(binary_search(r, 101));
